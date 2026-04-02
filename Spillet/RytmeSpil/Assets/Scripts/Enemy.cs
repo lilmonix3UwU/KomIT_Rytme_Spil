0abc94b86ec435d09b70233f3618c726264f50dd
lilmonix3UwU <115614969+lilmonix3UwU@users.noreply.github.com>
2026-04-02 19:10:33 +0200

    virker for det meste

--- Spillet/RytmeSpil/Assets/Scripts/Enemy.cs
+++ Spillet/RytmeSpil/Assets/Scripts/Enemy.cs
@@ -8,20 +8,25 @@ public class Enemy : MonoBehaviour
     public float HP;
     [SerializeField] TMP_Text dmgText;
     AudioSource hitSFX;
+    bool dying = false;
     private void Start()
     {
         hitSFX = GetComponent<AudioSource>();
     }
     public IEnumerator TakeDamage(float damage)
     {
+        if (dying)
+            yield break;
         hitSFX.Stop();
         hitSFX.Play();
         HP -= damage;
         dmgText.text = damage.ToString();
         dmgText.gameObject.SetActive(true);
+        if (HP <= 0)
+            dying = true;
         yield return new WaitForSeconds(0.3f);
         dmgText.gameObject.SetActive(false);
-        if (HP <= 0)
+        if (dying)
         {
             Destroy(gameObject);
         }
--- Spillet/RytmeSpil/Assets/Scripts/GroundCheck.cs
+++ Spillet/RytmeSpil/Assets/Scripts/GroundCheck.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public bool isGrounded;
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (!isGrounded && collision.gameObject.CompareTag("Ground"))
         {
